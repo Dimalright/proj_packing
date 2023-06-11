dfa67e6d3d42ab7923f2e5a0bcf47fe6029484ad
dimalright <yagam3333@gmail.com>
2023-06-11 17:36:01 +0300

    upd3

--- packing_service/packing_service/urls.py
+++ packing_service/packing_service/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .swagger import schema_view
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
