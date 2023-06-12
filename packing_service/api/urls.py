8d27852345557c7660b51344a9f2ae831cc9e660
dimalright <yagam3333@gmail.com>
2023-06-12 20:52:08 +0300

    red schema

--- packing_service/api/urls.py
+++ packing_service/api/urls.py
@@ -9,7 +9,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
         default_version="v1",
-        description="API documentation for your project",
+        description="API documentation",
         terms_of_service="https://www.example.com/policies/terms/",
         contact=openapi.Contact(email="contact@example.com"),
         license=openapi.License(name="BSD License"),
