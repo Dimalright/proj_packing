5eb48e173bad409e39ede9184893d3d053a7d7a6
dimalright <yagam3333@gmail.com>
2023-06-14 22:24:02 +0300

    red setting database

--- packing_service/packing_service/settings.py
+++ packing_service/packing_service/settings.py
@@ -67,15 +67,10 @@ WSGI_APPLICATION = "packing_service.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'team4',
-        'USER': 'postgres',
-        'PASSWORD': '331188',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
